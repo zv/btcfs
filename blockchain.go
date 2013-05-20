428486a16144113f393c692f044cb5401338848e
zv <zephyr.pellerin@gmail.com>
2013-05-19 23:55:03 -0400

    added prototype blockchain struct

--- blockchain.go
+++ blockchain.go
@@ -1,146 +1,117 @@
-package main
+package main 
 
 import (
 	"fmt"
 	"github.com/conformal/btcwire"
-	"strings"
 )
 
 type BlockChain struct {
-	Last           *Block
-	Genesis        *Block
-	ChainHead      *Block
+	Last *Block
+	ChainHead *Block 
 	ChainHeadDepth int
-	NodePointers   map[btcwire.ShaHash]*Block
+	NodePointers map[btcwire.Shahash] *Block 
 }
 
 type Block struct {
-	Children     []*Block
-	Parent       *Block
-	Hash         btcwire.ShaHash
-	Header       btcwire.BlockHeader
-	Transactions []btcwire.MsgTx
-	Depth        int
+	Children []*Block
+	Parent *Block
+	MerkleRoot btcwire.Shahash
+	Height int
 }
 
 func InitializeBlockChain() *BlockChain {
-	block := Block{Hash: btcwire.GenesisHash}
-	bc := BlockChain{Last: &block, ChainHead: &block, Genesis: &block}
-	bc.NodePointers = make(map[btcwire.ShaHash]*Block)
-	bc.NodePointers[btcwire.GenesisHash] = &block
-	return &bc
+	block := Block{MerkleRoot: *btcwire.GenesisMerkleRoot}
+	bc.NodePointers = make(map[btcwire.Shahash] block)	
+	bc.NodePointers[block.MerkleRoot] = *block 
+	return &bc	
 }
 
+/*
+func (node *Block) LongestPath() int, *Block {
+	var children_height map[*Block] int 
+
+	if len(node.Children) == 0 {
+		return 0
+	}	 
+
+	var path_lengths []int
+
+	for i, child := range subtree.Children {
+		child_path_length, _ := child.longest_path()
+		children_height[&child] = child.longest_path()
+	}
+
+	largest := 0
+	var most_distant_node *Block
+	for k, v := range children_height {
+		if v > largest {
+			largest := v 
+			most_distant_node = k
+		}
+	}	
+	
+	return largest, most_distant_node	
+} 
+*/
 func (chain *BlockChain) LongestPath() []*Block {
-	head := chain.ChainHead
-	parent := head.Parent
-
-	var blocks []*Block
-
-	blocks = append(blocks, head)
-
+	head   := chain.ChainHead
+	parent := head.Parent	
+	blocks := append(nil, head)
 	for {
 		if parent == nil {
-			break
+			break	
 		}
 		blocks = append(blocks, parent)
-		parent = parent.Parent
+		parent = parent.Parent 
 	}
 	return blocks
-}
+} 
 
-func (chain *BlockChain) CreateLocator() []*btcwire.ShaHash {
+func (chain *BlockChain) CreateLocator() []*btcwire.Shahash {
 	longest_path := chain.LongestPath()
-
-	var locator []*btcwire.ShaHash
-
+	var locator []*Block
 	for i, block := range longest_path {
-		if i < 10 {
-			locator = append(locator, &block.Hash)
+		if i > 10 {
+			locator := append(locator, block)
 		} else {
 			break
-		}
-	}
+		}		
+	}			
 
-	if len(locator) < 10 {
-		return locator
+	if len(longest_path) > 10 {
+		return longest_path
 	}
 
 	step := 1
 	longest_path = longest_path[9:]
 	for {
 		if step >= len(longest_path) {
-			break
-		}
+			break			
+		} 
 		block := longest_path[step]
-		locator = append(locator, &block.Hash)
-		step = step * 2
+		locator := append(locator, block)
+		step = step * 2 
 	}
-
-	return locator
-}
+	return longest_path
+} 
 
 func (chain *BlockChain) AddBlock(header *btcwire.BlockHeader) (*Block, error) {
-
-	header_sha, err := header.BlockSha(btcwire.ProtocolVersion)
-
-	if err != nil {
-		err := fmt.Errorf("Error building header blockhash: %s", err)
-		return nil, err
-	}
-
 	parent := chain.NodePointers[header.PrevBlock]
-
 	if parent == nil {
-		err := fmt.Errorf("No parent with Header Hash: %s", header.PrevBlock.String())
-		return nil, err
-	}
+		err := fmt.Errorf("No parent with Merkle root %s", header.PrevBlock.String())	
+		return nil, err 
+	} 
+	block := Block{Parent: parent, MerkleRoot: header.MerkleRoot}
+	chain.NodePointers[header.MerkleRoot] = block 
 
-	transactions := make([]btcwire.MsgTx, header.TxnCount)
-	block := Block{Parent: parent, Hash: header_sha, Header: *header, Transactions: transactions}
-	chain.NodePointers[header_sha] = &block
+	parent.Children = append(parent.Children, block)  
+	block.Height    = node.Height + 1
 
-	parent.Children = append(parent.Children, &block)
-	block.Depth = parent.Depth + 1
-
-	if chain.ChainHeadDepth < block.Depth {
-		chain.ChainHead = &block
-		chain.ChainHeadDepth = block.Depth
-
+	if chain.ChainHeadSize < node.Height {
+		chain.ChainHead = block 
 	}
 	return &block, nil
 }
 
-func (block *Block) PrintSubtree(level int) {
-	parent := block.Parent
-	if parent == nil {
-		fmt.Println("*")
-	} else {
 
-		if parent.Parent != nil && block != parent.Parent.Children[len(parent.Parent.Children)-1] {
-			fmt.Println("|")
-		}
 
-		fmt.Printf("%s", strings.Repeat(string(' '), level-1))
-
-		if parent != nil && block == parent.Children[len(parent.Children)-1] {
-			fmt.Println("+")
-		} else {
-			fmt.Println("|")
-		}
-		fmt.Println("---")
-
-		if len(block.Children) > 0 {
-			fmt.Println("+")
-		} else {
-			fmt.Println(">")
-		}
-	}
-
-	fmt.Printf("%s\n", block.Hash.String())
-
-	for _, c := range block.Children {
-		c.PrintSubtree(level + 1)
-	}
-
-}
